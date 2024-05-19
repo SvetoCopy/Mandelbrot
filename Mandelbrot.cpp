dc7122e853115ce97508464e8d93e8dd22fe8743
SvetoCopy <mrfairow@yandex.com>
2024-05-19 23:34:30 +0300

    avx mandelbrot

--- Mandelbrot.cpp
+++ Mandelbrot.cpp
@@ -1,4 +1,5 @@
 #include "Mandelbrot.h"
+#include <immintrin.h>
 
 Mandelbrot::Mandelbrot(size_t win_width, size_t win_height) : 
             height{win_height}, 
@@ -20,40 +21,63 @@ void Mandelbrot::SetPixel(size_t x, size_t y, sf::Color color)
 
 void Mandelbrot::CalculatePixels(float x_center, float y_center, float scale)
 {
-    const int n_max = 256;
-    const float r2_max = 100.f;
+    const int   n_max = 256;
     const float dx = 1/800.f * scale, dy = 1/800.f * scale;
+
+    const __m512 r2_max = _mm512_set1_ps(100.f);
+    const __m512 N_max  = _mm512_set1_ps(n_max);
     
     for (int iy = 0; iy < this->height; iy++)
     {
         float x0 = ((            - (float)this->width)  * dx) + x_center;
         float y0 = (((float)iy   - (float)this->height) * dy) + y_center;
 
-        for (int ix = 0; ix < this->width; ix++, x0 += dx)
+        for (int ix = 0; ix < this->width; ix += 16, x0 += 16 * dx)
         {
-            float X = x0;
-            float Y = y0;
+            __m512 range_15_to_0 = _mm512_set_ps(range_15_to_0_nums);
+            
+            __m512 X0 = _mm512_add_ps(_mm512_set1_ps(x0), _mm512_mul_ps(range_0_to_15, _mm512_set1_ps(dx)));
+            __m512 Y0 = _mm512_set1_ps(y0); 
 
-            int N = 0;
-            for (; N < n_max; N++)
+            __m512 X = X0; 
+            __m512 Y = Y0; 
+
+            __m512i N = _mm512_setzero_si512();
+
+            for (int n = 0; n < n_max; n++)
             {
-                float x2 = X*X;
-                float y2 = Y*Y;
-                float xy = X*Y;
+                __m512 x2 = _mm512_mul_ps(X, X);
+                __m512 y2 = _mm512_mul_ps(Y, Y);
+                __m512 xy = _mm512_mul_ps(X, Y);
 
-                float r2 = x2 + y2;
+                __m512 r2 = _mm512_mul_ps(x2, y2);
 
-                if (r2 >= r2_max) break;
+                __mmask16 cmp = _mm512_cmple_ps_mask(r2, r2_max);
+                
+                unsigned int mask = _cvtmask16_u32(cmp);
+                if (!mask) break;
 
-                X = x2 - y2 + x0;
-                Y = 2*xy    + y0;
+                __m512i cnst_1 = _mm512_set1_epi32(1);
+
+                N = _mm512_mask_add_epi32(N, cmp, N, cnst_1);
+                // for (int i = 0; i < 4; i++) N[i] = N[i] + cmp[i];
+
+                X = _mm512_add_ps(_mm512_sub_ps(x2, y2), X0); // X[i] = x2[i] - y2[i] + X0[i]
+                Y = _mm512_add_ps(_mm512_add_ps(xy, xy), Y0); // Y[i] = 2 * xy[i]  + Y0[i]
             }
 
-            float I = sqrtf(sqrtf ((float)N / (float)n_max)) * 255.f;
+            __m512 cnst_255 = _mm512_set1_ps(255.f);
+            __m512 shade = _mm512_mul_ps (_mm512_sqrt_ps(_mm512_sqrt_ps (_mm512_div_ps (_mm512_cvtepi32_ps (N), N_max))), cnst_255);
 
-            char c = (char)I;
-            sf::Color color(c, c, c%2*64);
-            this->SetPixel(ix, iy, color);
+            for (int i = 0; i < 16; i++) {
+
+                float* p_shade = (float*)&shade; 
+                
+                char c = (char)p_shade[i];
+                sf::Color color(c, c, c % 2 * 64);
+                this->SetPixel(ix + i, iy, color);
+            }
+
         }
     }
 
@@ -67,9 +91,9 @@ void Mandelbrot::CalculatePixels(float x_center, float y_center, float scale)
 
 void Mandelbrot::Display()
 {
-    float x_center = 0.f;
-    float y_center = 0.f;
-    float  scale    = 1.f;
+    float x_center = INIT_X_CENTER;
+    float y_center = INIT_Y_CENTER;
+    float scale    = INIT_SCALE;
 
     this->CalculatePixels(x_center, y_center, scale);
     this->window.display();
@@ -97,8 +121,9 @@ void Mandelbrot::Display()
                 else if (event.key.code == sf::Keyboard::A)
                     scale += 0.1;
                 
-                else if (event.key.code == sf::Keyboard::Z)
+                else if (event.key.code == sf::Keyboard::Z && (scale - 0.1) > 0){
                     scale -= 0.1;
+                }
 
                 else if (event.key.code == sf::Keyboard::Escape)
                     this->window.close();
--- Mandelbrot.h
+++ Mandelbrot.h
@@ -2,13 +2,22 @@
 #include <SFML/Graphics.hpp>
 #include <math.h>
 
+#define range_15_to_0_nums 15, 14, 13, 12, 11, 10, 9, 8, 7, 6 , 5, 4, 3, 2, 1, 0
+
 const float HORIZONTAL_SHIFT_AMOUNT = 0.2f;
 const float VERTICAL_SHIFT_AMOUNT   = 0.2f;
 
+const float INIT_X_CENTER = 0.f;
+const float INIT_Y_CENTER = 0.f;
+const float INIT_SCALE    = 1.f;
+
+const size_t INIT_WIDTH  = 100;
+const size_t INIT_HEIGHT = 800;
+
 class Mandelbrot {
 public:
-    size_t width  = 100;
-    size_t height = 800;
+    size_t width  = INIT_WIDTH;
+    size_t height = INIT_HEIGHT;
 
     Mandelbrot(size_t win_width, size_t win_height);
 
